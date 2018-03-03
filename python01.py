982cf806a32dfa31aa9faad9b57427e62cf65172
Shu <shu@MotovilovAA>
2018-03-03 11:33:22 +1000

    Add fiz address and some small fixes

--- python01.py
+++ python01.py
@@ -1,5 +1,6 @@
 import re
 import os
+from transliterate import translit
 
 # -*- coding: utf-8 -*-
 interf = input('Введите интерфейс \n')
@@ -8,13 +9,13 @@ svlan = input('Введите Svlan \n')
 cvlan = input('Введите inner-vlan \n')
 cpe = input('Введите hostname CPE \n')
 cpeport = input('Введите порт на CPE \n')
-family = input('Введите ФИО \n')
-pp = input('Введите №ПП')
-bw = input('Название скоростного полисера на терминации "limXm"')
-date = os.popen("date +%d.%m.%Y").read() 
+#family = input('Введите ФИО \n')
+pp = input('Введите №ПП \n')
+bw = input('Название скоростного полисера на терминации "limXm" \n')
+date = (os.popen("date +%d.%m.%Y").read()).strip()
 #comandcpip = print('ping' + ' ' +  cpe + '-c 1 | grep PING | awk \'{print $3}\'')
-cpeip = os.popen("ping " + cpe " -c 1 | grep PING | awk '{print $3}'").read()
-
+#cpeip = os.popen("ping " + cpe " -c 1 | grep PING | awk '{print $3}'").read()
+DATE = str(date) 
 
 resultlist = []
 vardict = {}
@@ -34,7 +35,9 @@ with open('temp01', 'r') as f:
         ipad = re.search('(([0-9]+\.)+[0-9]+)\s+Адрес\sРОСТЕЛЕКОМ', line)
         cms = re.search('(CMS\S)\s*(\d+\S\d+\S\d?)', line)
         surms = re.search('Дата и номер:\s*(\d+\.)+\d+\s\S(\d+)', line)
-        vpn = re.search('\S+\sVPN:\s(\S+)\s\(\S+\)', line)
+        vpn = re.search('^VPN ID:\s(\d*)', line)
+        fizaddr = re.search('^68\d{4}.\s(.*)', line)
+        clientname = re.search('^Тема:\s(.*)', line) 
         if ipmask:
              ipmask = ipmask.group(2)
              vardict['ipmask'] = str(ipmask)
@@ -50,20 +53,29 @@ with open('temp01', 'r') as f:
         elif vpn:
              vpn = vpn.group(1)
              vardict['vpn'] = str(vpn)
+        elif fizaddr:
+             fizaddr = translit(fizaddr.group(1), reversed=True)
+             vardict['fizaddr'] = str(fizaddr)
+        elif clientname:
+             clientname = translit(clientname.group(1), reversed=True)
+             vardict['clientname'] = str((clientname.lower()).capitalize())
         else:
              continue 
-#print(vardict)
-#resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'description \"##XXX SURMS:' + vardict.get('surms') + ' ' + vardict.get('cms') + ' '+ 'pp' + pp + ' ' + '##\"')
-resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'description \"#' + vardict.get('vpn') + ',' + ' ' + 'ClientXXX' + ' ' + 'SURMS:' + vardict.get('surms')+ ',' + ' ' + date + ' ' + '|' + ' ' + 'SVL:' + svlan + ' ' + 'CVL:' + cvlan + ',' + 'GOROD, ULICA' + ' ' + '|' + ' ' + cpe + ':' + cpeport + ' ' +  '|' + ' ' + 'pp' + pp + ' ' + '|' + ' ' + family +' ' + '#\"')
-resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'vlan-tags outer' + ' ' + svlan)
+print(vardict)
+print('-'*100)
+if vardict.get('vpn'):
+    resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'description \"##VPN:v' + vardict.get('vpn') + ',' + ' ' + vardict.get('clientname') + ', ' + 'SURMS:' + vardict.get('surms')+ ',' + ' ' + DATE + ' ' + '|' + ' ' + 'SVL:' + svlan + ' ' + 'CVL:' + cvlan + ',' + ' ' + vardict.get('fizaddr') + ' ' + '|' + ' ' + cpe + ':' + cpeport + ' ' +  '|' + ' ' + 'pp' + pp + ' ' + '|' + ' ' + 'Motovilov' +' ' + '##\"')
+else:
+    resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'description \"##INET,' + ' ' + vardict.get('clientname') + ', ' + 'SURMS:' + vardict.get('surms')+ ',' + ' ' + DATE + ' ' + '|' + ' ' + 'SVL:' + svlan + ' ' + 'CVL:' + cvlan + ',' + ' ' + vardict.get('fizaddr') + ' ' + '|' + ' ' + cpe + ':' + cpeport + ' ' + '|' + ' ' + 'pp' + pp + ' ' + '|' + ' ' + 'Motovilov' +' ' + '##\"')
 resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'vlan-tags outer' + ' ' + svlan)
 resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'vlan-tags inner' + ' ' + cvlan)
 resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'family inet rpf-check')
-#resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'family inet policer input lim' + vardict.get('speed') + bw)
-#resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'family inet policer output lim' + vardict.get('speed') + bw)
+resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'family inet policer input ' +  bw)
+resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'family inet policer output ' +  bw)
 resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'family inet address' + ' ' + vardict.get('ipad') + vardict.get('ipmask'))
 if vardict.get('vpn'):
-    resultlist.append('set routing-instances' + ' ' + vardict.get('vpn') + ' ' + 'interface' + ' ' + interf + '.' + uni) 
+    resultlist.append('set routing-instances' + ' ' + 'v' + vardict.get('vpn') + '####Здесь допиши имя инстанса!!!####' + ' ' + 'interface' + ' ' + interf + '.' + uni  ) 
 else:
     pass 
-print('\n'.join(resultlist))        
+print('\n'.join(resultlist))       
+
