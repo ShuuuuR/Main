f450067a1b772cbcaf72824080373a58fbb6055a
Shu <shu@MotovilovAA>
2018-03-07 16:37:39 +1000

    Add routiong instance

--- python01.py
+++ python01.py
@@ -1,5 +1,8 @@
 import re
 import os
+import paramiko
+import getpass
+import time
 from transliterate import translit
 
 # -*- coding: utf-8 -*-
@@ -53,8 +56,33 @@ with open('temp01', 'r') as f:
              surms = surms.group(2)
              vardict['surms'] = str(surms)
         elif vpn:
-             vpn = vpn.group(1)
-             vardict['vpn'] = str(vpn)
+             vpn = 'v' + vpn.group(1)
+             vpn = str(vpn)
+             #print(vpn)
+             command = 'show configuration routing-instances | match ' + vpn + ' | display set | match instance-type'
+             USER_S14 = input('Username_for_S14: ')
+             PASSWORD_S14 = getpass.getpass(prompt='Password_for_S14:')
+             S14_IP = '#####IPADDR#####'
+             sshS14 = paramiko.SSHClient()
+             sshS14.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+             sshS14.connect(hostname=S14_IP, username=USER_S14, password=PASSWORD_S14, look_for_keys=False, allow_agent=False)
+             with sshS14.invoke_shell() as ssh:
+                USER_BPE1 = input('Username_for_BPE1: ')
+                PASSWORD_BPE1 = getpass.getpass(prompt='Password_for_BPE1:')
+                ssh.send('telnet #####IPADDR#####\n')
+                time.sleep(1)
+                ssh.send(USER_BPE1 +'\n')
+                time.sleep(1)
+                ssh.send(PASSWORD_BPE1 + '\n')
+                time.sleep(3)
+                ssh.recv(1000)
+                ssh.send(command + '\n')
+                time.sleep(3)
+                result = ssh.recv(5000).decode('utf-8')
+             #получил нужное значение регекспом, но нужно придумать как сделать так, чтобы вывод парамико уже был в одну строку.
+                vpn_name = re.search('.*set routing-instances (v\d{4}-\S+)\s.*', result)
+                vpn_full_name = vpn_name.group(1)
+                vardict['vpn_full_name'] = str(vpn_full_name)
         elif clientname:
              clientname = translit(clientname.group(1), reversed=True)
              vardict['clientname'] = str((clientname.lower()).capitalize())
@@ -63,8 +91,9 @@ with open('temp01', 'r') as f:
 #print(vardict.get('fizaddr'))
 #print(vardict)
 print('-'*100)
-if vardict.get('vpn'):
-    resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'description \"##VPN:v' + vardict.get('vpn') + ',' + ' ' + vardict.get('clientname') + ', ' + 'SURMS:' + vardict.get('surms')+ ',' + ' ' + DATE + ' ' + '|' + ' ' + 'SVL:' + svlan + ' ' + 'CVL:' + cvlan + ',' + ' ' + vardict.get('fizaddr') + ' ' + '|' + ' ' + cpe + ':' + cpeport + ' ' +  '|' + ' ' + 'pp' + pp + ' ' + '|' + ' ' + 'Motovilov' +' ' + '##\"')
+if vardict.get('vpn_full_name'):
+    resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'description \"##VPN:' + vardict.get('vpn_full_name') + ',' + ' ' + vardict.get('clientname') + ', ' + 'SURMS:' + vardict.get('surms')+ ',' + ' ' + DATE + ' ' + '|' + ' ' + 'SVL:' + svlan + ' ' + 'CVL:' + cvlan + ',' + ' ' + vardict.get('fizaddr') + ' ' + '|' + ' ' + cpe + ':' + cpeport + ' ' +  '|' + ' ' + 'pp' + pp + ' ' + '|' + ' ' + 'Motovilov' +' ' + '##\"')
+    resultlist.append('set routing-instances' + ' ' + vardict.get('vpn_full_name') + ' ' + 'interface' + ' ' + interf + '.' + uni  )
 else:
     resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'description \"##INET,' + ' ' + vardict.get('clientname') + ', ' + 'SURMS:' + vardict.get('surms')+ ',' + ' ' + DATE + ' ' + '|' + ' ' + 'SVL:' + svlan + ' ' + 'CVL:' + cvlan + ',' + ' ' + vardict.get('fizaddr') + ' ' + '|' + ' ' + cpe + ':' + cpeport + ' ' + '|' + ' ' + 'pp' + pp + ' ' + '|' + ' ' + 'Motovilov' +' ' + '##\"')
 resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'vlan-tags outer' + ' ' + svlan)
@@ -73,9 +102,9 @@ resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + '
 resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'family inet policer input ' +  bw)
 resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'family inet policer output ' +  bw)
 resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'family inet address' + ' ' + vardict.get('ipad') + vardict.get('ipmask'))
-if vardict.get('vpn'):
-    resultlist.append('set routing-instances' + ' ' + 'v' + vardict.get('vpn') + '####Здесь допиши имя инстанса!!!####' + ' ' + 'interface' + ' ' + interf + '.' + uni  ) 
-else:
-    pass 
+#if vardict.get('vpn'):
+#    resultlist.append('set routing-instances' + ' ' + 'v' + vardict.get('vpn') + '####Здесь допиши имя инстанса!!!####' + ' ' + 'interface' + ' ' + interf + '.' + uni  ) 
+#else:
+#    pass 
 print('\n'.join(resultlist))       
 
