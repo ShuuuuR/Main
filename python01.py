053dd6a1601a3ff737623d8429a8864e5ce53e5b
Shu <shu@MotovilovAA>
2018-03-26 16:16:06 +1000

    Add some L2 functionality

--- python01.py
+++ python01.py
@@ -70,7 +70,7 @@ with open('temp01', 'r') as f:
         surms = re.search('Дата и номер:\s*(\d+\.)+\d+\s\S(\d+)', line)
         vpn = re.search('^VPN ID:\s(\d*)', line)
         clientname = re.search('^Тема:\s(.*)', line) 
-        l2vpn = re.search('^.*L2 VPN', line)
+        l2vpn = re.search('^.*L2 VPN.*', line)
         if ipmask:
              ipmask = ipmask.group(2)
              vardict['ipmask'] = str(ipmask)
@@ -88,15 +88,19 @@ with open('temp01', 'r') as f:
              vpn = str(vpn)
              command_0 = 'show configuration routing-instances | match ' + vpn + ' | display set | match instance-type'
              command_1 = 'show configuration interfaces ' + interf + ' | match "unit " | no-more' + '\n'
-             USER_S14 = input('Username_for_S14: ')
-             PASSWORD_S14 = getpass.getpass(prompt='Password_for_S14:')
+             #USER_S14 = input('Username_for_S14: ')
+             USER_S14 = 'user'
+             #PASSWORD_S14 = getpass.getpass(prompt='Password_for_S14:')
+             PASSWORD_S14 = 'PASS'
              S14_IP = '###IPADDR_S14####'
              sshS14 = paramiko.SSHClient()
              sshS14.set_missing_host_key_policy(paramiko.AutoAddPolicy())
              sshS14.connect(hostname=S14_IP, username=USER_S14, password=PASSWORD_S14, look_for_keys=False, allow_agent=False)
              with sshS14.invoke_shell() as ssh:
-                 USER_BPE1 = input('Username_for_BPE1: ')
-                 PASSWORD_BPE1 = getpass.getpass(prompt='Password_for_BPE1:')
+                 #USER_BPE1 = input('Username_for_BPE1: ')
+                 USER_BPE1 = 'USER'
+                 #PASSWORD_BPE1 = getpass.getpass(prompt='Password_for_BPE1:')
+                 PASSWORD_BPE1 = 'PASS'
                  ssh.send('telnet <####IPADDR_BPE1####>\n')
                  time.sleep(1)
                  ssh.send(USER_BPE1 +'\n')
@@ -127,35 +131,26 @@ with open('temp01', 'r') as f:
              #print(clientname.group(1))
              clientname = translit(clientname.group(1), reversed=True)
              vardict['clientname'] = str(short_names((clientname.lower()).capitalize()))
+        elif l2vpn:
+             vardict['l2vpn'] = str(l2vpn.group())
         else:
              continue 
-#print(vardict.get('fizaddr'))
-#print(vardict)
 print('-'*100)
-if l2vpn:
-   resultlist.append('set interfaces' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'description \"##L2VPN,' + ' ' + vardict.get('clientname') + ',' + 'SURMS:' + vardict.get('surms')+ ',' + ' ' + DATE + ' ' + '|' + ' ' + 'SVL:' + svlan + ' ' + 'CVL:' + cvlan + ',' + ' ' + vardict.get('fizaddr') + ' ' + '|' + ' ' + cpe + ':' + cpeport + ' ' +  '|' + ' ' + 'pp' + pp + ' ' + '|' + ' ' + 'Motovilov' +' ' + '##\"') 
-   resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'apply-groups-except arp-police')
-   resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'encapsulation vlan-ccc')
-   resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'input-vlan-map pop-pop')
-   resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'input-vlan-map push-push')
-
-
-elif vardict.get('vpn_full_name'):
-    resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'description \"##VPN:' + vardict.get('vpn_full_name') + ',' + ' ' + vardict.get('clientname') + ', ' + 'SURMS:' + vardict.get('surms')+ ',' + ' ' + DATE + ' ' + '|' + ' ' + 'SVL:' + svlan + ' ' + 'CVL:' + cvlan + ',' + ' ' + vardict.get('fizaddr') + ' ' + '|' + ' ' + cpe + ':' + cpeport + ' ' +  '|' + ' ' + 'pp' + pp + ' ' + '|' + ' ' + 'Motovilov' +' ' + '##\"')
-    resultlist.append('set routing-instances' + ' ' + vardict.get('vpn_full_name') + ' ' + 'interface' + ' ' + interf + '.' + uni  )
-    resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'family inet rpf-check')
-    resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'family inet address' + ' ' + vardict.get('ipad') + vardict.get('ipmask'))
-else:
+if vardict.get('l2vpn'):
     command_1 = 'show configuration interfaces ' + interf + ' | match "unit " | no-more' + '\n'
-    USER_S14 = input('Username_for_S14: ')
-    PASSWORD_S14 = getpass.getpass(prompt='Password_for_S14:')
+    #USER_S14 = input('Username_for_S14: ')
+    USER_S14 = 'user'
+    #PASSWORD_S14 = getpass.getpass(prompt='Password_for_S14:')
+    PASSWORD_S14 = 'PASS'
     S14_IP = '###IPADDR_S14####'
     sshS14 = paramiko.SSHClient()
     sshS14.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     sshS14.connect(hostname=S14_IP, username=USER_S14, password=PASSWORD_S14, look_for_keys=False, allow_agent=False)
     with sshS14.invoke_shell() as ssh:
-        USER_BPE1 = input('Username_for_BPE1: ')
-        PASSWORD_BPE1 = getpass.getpass(prompt='Password_for_BPE1:')
+        #USER_BPE1 = input('Username_for_BPE1: ')
+        USER_BPE1 = 'USER'
+        #PASSWORD_BPE1 = getpass.getpass(prompt='Password_for_BPE1:')
+        PASSWORD_BPE1 = 'PASS'
         ssh.send('telnet <####IPADDR_BPE1####>\n')
         time.sleep(1)
         ssh.send(USER_BPE1 +'\n')
@@ -172,17 +167,60 @@ else:
             if str(line) not in unit_num_list:
                 free_unit_list.append(str(line))
         uni = free_unit_list[4]
-   # print(uni)
-   # print(vardict.get('fizaddr'))
-   # print(vardict.get('clientname'))
-   # print(uni)
-   # print(vardict.get('surms'))
-   # print(DATE)
-   # print(svlan)
-   # print(cvlan)
-   # print(cpe)
-   # print(cpeport)
-   # print(pp)
+    resultlist.append('set interfaces' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'description \"##L2VPN,' + ' ' + vardict.get('clientname') + ',' + 'SURMS:' + vardict.get('surms')+ ',' + ' ' + DATE + ' ' + '|' + ' ' + 'SVL:' + svlan + ' ' + 'CVL:' + cvlan + ',' + ' ' + vardict.get('fizaddr') + ' ' + '|' + ' ' + cpe + ':' + cpeport + ' ' +  '|' + ' ' + 'pp' + pp + ' ' + '|' + ' ' + 'Motovilov' +' ' + '##\"') 
+    resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'apply-groups-except arp-police')
+    resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'encapsulation vlan-ccc')
+    resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'input-vlan-map pop-pop')
+    resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'input-vlan-map push-push')
+
+
+elif vardict.get('vpn_full_name'):
+    resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'description \"##VPN:' + vardict.get('vpn_full_name') + ',' + ' ' + vardict.get('clientname') + ', ' + 'SURMS:' + vardict.get('surms')+ ',' + ' ' + DATE + ' ' + '|' + ' ' + 'SVL:' + svlan + ' ' + 'CVL:' + cvlan + ',' + ' ' + vardict.get('fizaddr') + ' ' + '|' + ' ' + cpe + ':' + cpeport + ' ' +  '|' + ' ' + 'pp' + pp + ' ' + '|' + ' ' + 'Motovilov' +' ' + '##\"')
+    resultlist.append('set routing-instances' + ' ' + vardict.get('vpn_full_name') + ' ' + 'interface' + ' ' + interf + '.' + uni  )
+    resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'family inet rpf-check')
+    resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'family inet address' + ' ' + vardict.get('ipad') + vardict.get('ipmask'))
+else:
+    command_1 = 'show configuration interfaces ' + interf + ' | match "unit " | no-more' + '\n'
+    #USER_S14 = input('Username_for_S14: ')
+    USER_S14 = 'user'
+    #PASSWORD_S14 = getpass.getpass(prompt='Password_for_S14:')
+    PASSWORD_S14 = 'PASS'
+    S14_IP = '###IPADDR_S14####'
+    sshS14 = paramiko.SSHClient()
+    sshS14.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    sshS14.connect(hostname=S14_IP, username=USER_S14, password=PASSWORD_S14, look_for_keys=False, allow_agent=False)
+    with sshS14.invoke_shell() as ssh:
+        #USER_BPE1 = input('Username_for_BPE1: ')
+        USER_BPE1 = 'USER'
+        #PASSWORD_BPE1 = getpass.getpass(prompt='Password_for_BPE1:')
+        PASSWORD_BPE1 = 'PASS'
+        ssh.send('telnet <####IPADDR_BPE1####>\n')
+        time.sleep(1)
+        ssh.send(USER_BPE1 +'\n')
+        time.sleep(1)
+        ssh.send(PASSWORD_BPE1 + '\n')
+        time.sleep(3)
+        ssh.recv(1000)
+        ssh.send(command_1 + '\n')
+        time.sleep(3)
+        result1 = ssh.recv(5000).decode('utf-8')
+        unit_num_list = re.findall('unit (\d+) {', result1)
+        free_unit_list =[] #создаем пустой список
+        for line in range(16384):
+            if str(line) not in unit_num_list:
+                free_unit_list.append(str(line))
+        uni = free_unit_list[4]
+    #print(uni)
+    #print(vardict.get('fizaddr'))
+    #print(vardict.get('clientname'))
+    #print(uni)
+    #print(vardict.get('surms'))
+    #print(DATE)
+    #print(svlan)
+    #print(cvlan)
+    #print(cpe)
+    #print(cpeport)
+    #print(pp)
 
     resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'description \"##INET,' + ' ' + vardict.get('clientname') + ', ' + 'SURMS:' + vardict.get('surms')+ ',' + ' ' + DATE + ' ' + '|' + ' ' + 'SVL:' + svlan + ' ' + 'CVL:' + cvlan + ',' + ' ' + vardict.get('fizaddr') + ' ' + '|' + ' ' + cpe + ':' + cpeport + ' ' + '|' + ' ' + 'pp' + pp + ' ' + '|' + ' ' + 'Motovilov' +' ' + '##\"')
     resultlist.append('set interfaces' + ' ' + interf + ' ' + 'unit' + ' ' + uni + ' ' + 'family inet rpf-check')
